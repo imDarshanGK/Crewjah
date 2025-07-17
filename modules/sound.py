8f5d965c8247f772da13e422d4b07e5e514dc681
LeonardoLBraga <Leonardobraga24@gmail.com>
2025-07-17 14:14:12 -0300

    docs: add docstrings on sound.py

--- modules/sound.py
+++ modules/sound.py
@@ -1,3 +1,17 @@
+"""
+sound.py
+
+Provides audio feedback functions for success and failure events using
+platform-specific sound libraries.
+
+Currently supports Windows only via the `winsound` module. On other platforms,
+the functions will silently do nothing.
+
+Functions:
+    play_success_sound(): Plays a system sound indicating success.
+    play_failure_sound(): Plays a system sound indicating failure.
+"""
+
 import platform
 
 try:
@@ -7,9 +21,23 @@ except ImportError:
     pass
 
 def play_success_sound():
+    """
+    Plays a system sound to indicate a successful action.
+
+    On Windows, this plays the 'asterisk' sound using the winsound module.
+    On other platforms, it does nothing.
+    """
+
     if platform.system() == "Windows":
         winsound.MessageBeep(winsound.MB_ICONASTERISK)
 
 def play_failure_sound():
+    """
+    Plays a system sound to indicate a failed action.
+
+    On Windows, this plays the 'hand' (error) sound using the winsound module.
+    On other platforms, it does nothing.
+    """
+
     if platform.system() == "Windows":
         winsound.MessageBeep(winsound.MB_ICONHAND)
